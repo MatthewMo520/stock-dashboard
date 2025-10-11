3cc8c6d04a9de6fb2374ab27f7bfe978e9f7628c
Matthew Mo <74524092+MatthewMo520@users.noreply.github.com>
2025-10-11 15:01:20 -0400

    fixed colours

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def daily_change_colour(val):
         colour = 'red'
     else:
         colour = 'black'
-    return f'colour: {colour}'
+    return f'color: {colour}'
 
 #----DISPLAY DATA TABLE----#
 st.subheader("Recent Data")
